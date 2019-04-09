ee32f2706cb5be72b3aed10c2b84c19ede3d676f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-09 13:15:42 +0200

    assigned key to clock

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <React.Fragment>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(val => (
-        <Clock />
+        <Clock key={val} />
       ))}
     </React.Fragment>
   );
