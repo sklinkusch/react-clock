8b160abf42bc09e4e07e7640595f356a9c9e9bd1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-09 11:49:11 +0200

    built a clock class (static)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Clock from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+  ReactDOM.render(<Clock />, document.getElementById("root"));
+}
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
