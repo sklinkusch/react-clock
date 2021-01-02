6522e068231f9c6ad867dedf7ada03aea22d7a78
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-02 15:24:56 +0100

    add West Africa

--- src/components/data.js
+++ src/components/data.js
@@ -24,6 +24,7 @@ import NorthwesternAtlantic from "./timezones/NorthwesternAtlantic"
 import Pacific from "./timezones/Pacific"
 import QuintanaRoo from "./timezones/QuintanaRoo"
 import Saskatchewan from "./timezones/Saskatchewan"
+import WestAfrica from "./timezones/WestAfrica"
 import WesternBrazil from "./timezones/WesternBrazil"
 import WesternEurope from "./timezones/WesternEurope"
 import WesternGreenland from "./timezones/WesternGreenland"
@@ -56,68 +57,11 @@ export default [
   Pacific,
   QuintanaRoo,
   Saskatchewan,
+  WestAfrica,
   WesternBrazil,
   WesternEurope,
   WesternGreenland,
   Yukon,
-  {
-    city: "West Africa",
-    zone: "Africa/Bamako",
-    flags: [
-      {
-        title: "Burkina Faso",
-        code: "BF"
-      },
-      {
-        title: "Côte d'Ivoire",
-        code: "CI"
-      },
-      {
-        title: "The Gambia",
-        code: "GM"
-      },
-      {
-        title: "Ghana",
-        code: "GH"
-      },
-      {
-        title: "Guinea",
-        code: "GN"
-      },
-      {
-        title: "Guinea-Bissau",
-        code: "GW"
-      },
-      {
-        title: "Liberia",
-        code: "LR"
-      },
-      {
-        title: "Mali",
-        code: "ML"
-      },
-      {
-        title: "Mauritania",
-        code: "MR"
-      },
-      {
-        title: "São Tomé and Príncipe",
-        code: "ST"
-      },
-      {
-        title: "Senegal",
-        code: "SN"
-      },
-      {
-        title: "Sierra Leone",
-        code: "SL"
-      },
-      {
-        title: "Togo",
-        code: "TG"
-      },
-    ]
-  },
   {
     city: "Central Europe",
     zone: "Europe/Berlin",
