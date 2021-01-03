4ecae99bb56ad8cfa255e7c4f2680d4bb186b735
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-03 18:19:23 +0100

    combine timezones for Mongolia

---
+++ src/components/timezones/Mongolia.js
@@ -0,0 +1,21 @@
+export const EasternMongolia = {
+  city: "Eastern Mongolia",
+  zone: "Asia/Ulan_Bator",
+  flags: [
+    {
+      title: "Mongolia: main part",
+      code: "MN"
+    }
+  ]
+}
+
+export const WesternMongolia = {
+  city: "Western Mongolia",
+  zone: "Asia/Hovd",
+  flags: [
+    {
+      title: "Mongolia: Bayan-Ölgii, Govi-Altai, Khovd, Uvs, Zavkhan",
+      code: "MN"
+    }
+  ]
+}
