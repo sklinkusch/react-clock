dd738a613732ec7d51facb4c92e14ebcd1934c34
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-03 15:37:04 +0100

    combine time zones for Africa

---
+++ src/components/timezones/Africa.js
@@ -0,0 +1,254 @@
+export const CentralAfrica = {
+  city: "Central Africa",
+  zone: "Africa/Cairo",
+  flags: [
+    {
+      title: "Botswana",
+      code: "BW"
+    },
+    {
+      title: "Burundi",
+      code: "BI"
+    },
+    {
+      title: "Democratic Republic of the Congo: Bas-Uele, Haut-Katanga, Haut-Lomami, Haut-Uele, Ituri, Kasaï, Kasaï-Central, Kasaï-Oriental, Lomami, Lualaba, Maniema, Nord-Kivu, Sankuru, Sud-Kivu, Tanganjika, Tshopo",
+      code: "CD"
+    },
+    {
+      title: "Egypt",
+      code: "EG"
+    },
+    {
+      title: "eSwatini",
+      code: "SZ"
+    },
+    {
+      title: "Lesotho",
+      code: "LS"
+    },
+    {
+      title: "Libya",
+      code: "LY"
+    },
+    {
+      title: "Malawi",
+      code: "MW"
+    },
+    {
+      title: "Mozambique",
+      code: "MZ"
+    },
+    {
+      title: "Namibia",
+      code: "NA"
+    },
+    {
+      title: "Rwanda",
+      code: "RW"
+    },
+    {
+      title: "South Africa",
+      code: "ZA"
+    },
+    {
+      title: "Sudan",
+      code: "SD"
+    },
+    {
+      title: "Zambia",
+      code: "ZM"
+    },
+    {
+      title: "Zimbabwe",
+      code: "ZW"
+    }
+  ]
+}
+
+export const EasternAfrica = {
+  city: "East Africa",
+  zone: "Africa/Addis_Ababa",
+  flags: [
+    {
+      title: "Comoros",
+      code: "KM"
+    },
+    {
+      title: "Djibouti",
+      code: "DJ"
+    },
+    {
+      title: "Eritrea",
+      code: "ER"
+    },
+    {
+      title: "Ethiopia",
+      code: "ET"
+    },
+    {
+      title: "Kenya",
+      code: "KE"
+    },
+    {
+      title: "Madagascar",
+      code: "MG"
+    },
+    {
+      title: "Somalia",
+      code: "SO"
+    },
+    {
+      title: "South Sudan",
+      code: "SS"
+    },
+    {
+      title: "Tanzania",
+      code: "TZ"
+    },
+    {
+      title: "Uganda",
+      code: "UG"
+    }
+  ]
+}
+
+export const IndianOcean = {
+  city: "Indian Ocean",
+  zone: "Indian/Reunion",
+  flags: [
+    {
+      title: "France: Réunion",
+      code: "RE"
+    },
+    {
+      title: "Mauritius",
+      code: "MU"
+    },
+    {
+      title: "Seychelles",
+      code: "SC"
+    },
+  ]
+}
+
+export const MiddleWestAfrica = {
+  city: "Middle West Africa",
+  zone: "Africa/Luanda",
+  flags: [
+    {
+      title: "Algeria",
+      code: "DZ"
+    },
+    {
+      title: "Angola",
+      code: "AO"
+    },
+    {
+      title: "Benin",
+      code: "BJ"
+    },
+    {
+      title: "Cameroon",
+      code: "CM"
+    },
+    {
+      title: "Central African Republic",
+      code: "CF"
+    },
+    {
+      title: "Chad",
+      code: "TD"
+    },
+    {
+      title: "Democratic Republic of the Congo: Équateur, Kinshasa, Kongo Central, Kwango, Kwilu, Mai-Ndombe, Mongala, Nord-Ubangi, Sud-Ubangi, Tshuapa",
+      code: "CD"
+    },
+    {
+      title: "Equatorial Guinea",
+      code: "GQ"
+    },
+    {
+      title: "Gabon",
+      code: "GA"
+    },
+    {
+      title: "Morocco",
+      code: "MA"
+    },
+    {
+      title: "Niger",
+      code: "NE"
+    },
+    {
+      title: "Nigeria",
+      code: "NG"
+    },
+    {
+      title: "Republic of the Congo",
+      code: "CG"
+    },
+    {
+      title: "Tunisia",
+      code: "TN"
+    },
+  ]
+}
+
+export const WestAfrica = {
+  city: "West Africa",
+  zone: "Africa/Bamako",
+  flags: [
+    {
+      title: "Burkina Faso",
+      code: "BF"
+    },
+    {
+      title: "Côte d'Ivoire",
+      code: "CI"
+    },
+    {
+      title: "The Gambia",
+      code: "GM"
+    },
+    {
+      title: "Ghana",
+      code: "GH"
+    },
+    {
+      title: "Guinea",
+      code: "GN"
+    },
+    {
+      title: "Guinea-Bissau",
+      code: "GW"
+    },
+    {
+      title: "Liberia",
+      code: "LR"
+    },
+    {
+      title: "Mali",
+      code: "ML"
+    },
+    {
+      title: "Mauritania",
+      code: "MR"
+    },
+    {
+      title: "São Tomé and Príncipe",
+      code: "ST"
+    },
+    {
+      title: "Senegal",
+      code: "SN"
+    },
+    {
+      title: "Sierra Leone",
+      code: "SL"
+    },
+    {
+      title: "Togo",
+      code: "TG"
+    },
+  ]
+}
