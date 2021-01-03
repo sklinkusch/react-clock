459a3ec1522a9bea33f14c4eedb35d4a1ca9dfeb
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-03 15:11:59 +0100

    combine timezones for North America

---
+++ src/components/timezones/NorthAmerica.js
@@ -0,0 +1,232 @@
+export const Alaska = {
+  city: "Alaska",
+  zone: "America/Anchorage",
+  flags: [
+    {
+      title: "French Polynesia (France): Gambier Islands",
+      code: "PF"
+    },
+    {
+      title: "United States: AK",
+      code: "US"
+    },
+  ]
+}
+
+export const AleutianIslands = {
+  city: "Aleutian Islands",
+  zone: "America/Atka",
+  flags: [
+    {
+      title: "United States: AK",
+      code: "US"
+    }
+  ]
+}
+
+export const Arizona = {
+  city: "Arizona",
+  zone: "America/Phoenix",
+  flags: [
+    {
+      title: "Mexico: Sonora, Revillagigedo Islands",
+      code: "MX"
+    },
+    {
+      title: "United States: AZ (excluding Navajo Nation Reservation)",
+      code: "US"
+    }
+  ]
+}
+
+export const AtlanticOcean = {
+  city: "Atlantic Ocean",
+  zone: "Atlantic/Azores",
+  flags: [
+    {
+      title: "Cabo Verde",
+      code: "CV"
+    },
+    {
+      title: "Greenland (Denmark)",
+      code: "GL"
+    },
+    {
+      title: "Portugal: Azores",
+      code: "PT"
+    },
+  ]
+}
+
+export const Central = {
+  city: "Central",
+  zone: "America/Chicago",
+  flags: [
+    {
+      title: "Canada: Manitoba, Nunavut, Ontario",
+      code: "CA"
+    },
+    {
+      title: "Mexico: Aguascalientes, Campeche, Chiapas, Coahuila, Colima, Distrito Federal, Durango, Guanajuato, Guerrero, Hidalgo, Jalisco, México, Michoacán, Morelos, Nuevo León, Oaxaca, Puebla, Querétaro, San Luis Potosí, Tabasco, Tamaulipas, Tlaxcala, Veracruz, Yucatán, Zacatecas",
+      code: "MX"
+    },
+    {
+      title: "United States: AL, AR, FL, IA, IL, IN, KS, KY, LA, MN, MO, MS, ND, NE, OK, SD, TN, TX, WI",
+      code: "US"
+    }
+  ]
+}
+
+export const Eastern = {
+  city: "Eastern",
+  zone: "America/New_York",
+  flags: [
+    {
+      title: "Bahamas",
+      code: "BS"
+    },
+    {
+      title: "Canada: Nunavut, Ontario, Québec",
+      code: "CA"
+    },
+    {
+      title: "Cuba",
+      code: "CU"
+    },
+    {
+      title: "United States: CT, DC, DE, FL, GA, IN, KY, MA, MD, ME, MI, NC, NH, NJ, NY, OH, PA, RI, SC, TN, VA, VT, WV",
+      code: "US"
+    },
+  ]
+}
+
+export const Hawaii = {
+  city: "Hawaii",
+  zone: "Pacific/Honolulu",
+  flags: [
+    {
+      title: "Cook Islands (New Zealand)",
+      code: "CK"
+    },
+    {
+      title: "French Polynesia (France): Austral Islands, Society Islands, Tuamotus",
+      code: "PF"
+    },
+    {
+      title: "United States: HI",
+      code: "US"
+    },
+  ]
+}
+
+export const Mountain = {
+  city: "Mountain",
+  zone: "America/Denver",
+  flags: [
+    {
+      title: "Canada: Alberta, British Columbia, Northwest Territories, Nunavut",
+      code: "CA"
+    },
+    {
+      title: "Mexico: Baja California Sur, Chihuahua, Nayarit, Sinaloa",
+      code: "MX"
+    },
+    {
+      title: "United States: AZ (only Navajo Nation Reservation), CO, ID, KS, MT, ND, NE, NM, OR, SD, TX, UT, WY",
+      code: "US"
+    }
+  ]
+}
+
+export const Newfoundland = {
+  city: "Newfoundland",
+  zone: "America/St_Johns",
+  flags: [
+    {
+      title: "Canada: Newfoundland",
+      code: "CA"
+    }
+  ]
+}
+
+export const NorthwesternAtlantic = {
+  city: "Northwestern Atlantic",
+  zone: "America/Halifax",
+  flags: [
+    {
+      title: "Canada: New Brunswick, Labrador, Nova Scotia, Prince Edward Island, Québec",
+      code: "CA"
+    },
+    {
+      title: "Greenland (Denmark)",
+      code: "GL"
+    },
+  ]
+}
+
+export const Pacific = {
+  city: "Pacific",
+  zone: "America/Los_Angeles",
+  flags: [
+    {
+      title: "Canada: British Columbia, Northwest Territories",
+      code: "CA"
+    },
+    {
+      title: "Mexico: Baja California",
+      code: "MX"
+    },
+    {
+      title: "United States: CA, ID, NV, OR, WA",
+      code: "US"
+    }
+  ]
+}
+
+export const QuintanaRoo = {
+  city: "Quintana Roo",
+  zone: "America/Cancun",
+  flags: [
+    {
+      title: "Mexico: Quintana Roo",
+      code: "MX"
+    }
+  ]
+}
+
+export const Saskatchewan = {
+  city: "Saskatchewan",
+  zone: "America/Regina",
+  flags: [
+    {
+      title: "Canada: Saskatchewan",
+      code: "CA"
+    }
+  ]
+}
+
+export const WesternGreenland = {
+  city: "Western Greenland",
+  zone: "America/Nuuk",
+  flags: [
+    {
+      title: "Greenland (Denmark)",
+      code: "GL",
+    },
+    {
+      title: "Saint-Pierre and Miquelon (France)",
+      code: "PM"
+    },
+  ]
+}
+
+export const Yukon = {
+  city: "Yukon",
+  zone: "America/Whitehorse",
+  flags: [
+    {
+      title: "Canada: Yukon",
+      code: "CA"
+    }
+  ]
+}
