391216aa866234f66d81890487b5bf4524d0ded0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-03 18:02:17 +0100

    combine time zones for South Asia

---
+++ src/components/timezones/SouthAsia.js
@@ -0,0 +1,66 @@
+export const Bangladesh = {
+  city: "Bangladesh",
+  zone: "Asia/Dhaka",
+  flags: [
+    {
+      title: "Bangladesh",
+      code: "BD"
+    },
+    {
+      title: "Bhutan",
+      code: "BT"
+    },
+  ]
+}
+
+export const India = {
+  city: "India",
+  zone: "Asia/Kolkata",
+  flags: [
+    {
+      title: "India",
+      code: "IN"
+    },
+    {
+      title: "Sri Lanka",
+      code: "LK"
+    }
+  ]
+}
+
+export const Myanmar = {
+  city: "Myanmar",
+  zone: "Asia/Rangoon",
+  flags: [
+    {
+      title: "Myanmar",
+      code: "MM"
+    }
+  ]
+}
+
+export const Nepal = {
+  city: "Nepal",
+  zone: "Asia/Kathmandu",
+  flags: [
+    {
+      title: "Nepal",
+      code: "NP"
+    }
+  ]
+}
+
+export const Pakistan = {
+  city: "Pakistan",
+  zone: "Asia/Karachi",
+  flags: [
+    {
+      title: "Maldives",
+      code: "MV"
+    },
+    {
+      title: "Pakistan",
+      code: "PK"
+    },
+  ]
+}
