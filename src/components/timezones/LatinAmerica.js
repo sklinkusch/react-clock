fe2f394a850a0311831f1acfc691cf37ca913920
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-01-03 15:58:56 +0100

    combine time zones for Latin America

---
+++ src/components/timezones/LatinAmerica.js
@@ -0,0 +1,126 @@
+export const CentralAmerica = {
+  city: "Central America",
+  zone: "America/El_Salvador",
+  flags: [
+    {
+      title: "Belize",
+      code: "BZ"
+    },
+    {
+      title: "Costa Rica",
+      code: "CR"
+    },
+    {
+      title: "Ecuador: Galápagos",
+      code: "EC"
+    },
+    {
+      title: "El Salvador",
+      code: "SV"
+    },
+    {
+      title: "Guatemala",
+      code: "GT"
+    },
+    {
+      title: "Honduras",
+      code: "HN"
+    },
+    {
+      title: "Nicaragua",
+      code: "NI"
+    },
+  ]
+}
+
+export const CentralSouthAmerica = {
+  city: "Central South America",
+  zone: "America/Caracas",
+  flags: [
+    {
+      title: "Bolivia",
+      code: "BO"
+    },
+    {
+      title: "Chile",
+      code: "CL"
+    },
+    {
+      title: "Falkland Islands",
+      code: "FK"
+    },
+    {
+      title: "Guyana",
+      code: "GY"
+    },
+    {
+      title: "Paraguay",
+      code: "PY"
+    },
+    {
+      title: "Venezuela",
+      code: "VE"
+    }
+  ]
+}
+
+export const Colombia = {
+  city: "Colombia",
+  zone: "America/Bogota",
+  flags: [
+    {
+      title: "Colombia",
+      code: "CO"
+    },
+    {
+      title: "Ecuador",
+      code: "EC"
+    },
+    {
+      title: "Panama",
+      code: "PA"
+    },
+    {
+      title: "Peru",
+      code: "PE"
+    },
+  ]
+}
+
+export const EasterIsland = {
+  city: "Easter Island",
+  zone: "Pacific/Easter",
+  flags: [
+    {
+      title: "Chile: Easter Island",
+      code: "CL"
+    },
+  ]
+}
+
+export const EasternSouthAmerica = {
+  city: "Eastern South America",
+  zone: "America/Buenos_Aires",
+  flags: [
+    {
+      title: "Argentina",
+      code: "AR"
+    },
+    {
+      title: "Chile: Magellan and Antarctic region",
+      code: "CL"
+    },
+    {
+      title: "French Guiana",
+      code: "GF"
+    },
+    {
+      title: "Suriname",
+      code: "SR"
+    },
+    {
+      title: "Uruguay",
+      code: "UY"
+    }
+  ]
+}
