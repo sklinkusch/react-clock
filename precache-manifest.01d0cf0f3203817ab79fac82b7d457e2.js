f82c3d25db52f9117f9a14624fc9251e15974753
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-13 22:07:56 +0200

    Updates

---
+++ precache-manifest.01d0cf0f3203817ab79fac82b7d457e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/react-clock/static/media/Digital7Mono-B1g5.58045dab.ttf"
+  },
+  {
+    "revision": "ce4bc6fe0ba94a305fa3",
+    "url": "/react-clock/static/js/runtime~main.dd06bf16.js"
+  },
+  {
+    "revision": "5697e924052cdaf8a7c9",
+    "url": "/react-clock/static/js/main.c0f03d1a.chunk.js"
+  },
+  {
+    "revision": "b4215a6755c75f8f76f2",
+    "url": "/react-clock/static/js/2.4110b4f7.chunk.js"
+  },
+  {
+    "revision": "5697e924052cdaf8a7c9",
+    "url": "/react-clock/static/css/main.1e2848e7.chunk.css"
+  },
+  {
+    "revision": "b4215a6755c75f8f76f2",
+    "url": "/react-clock/static/css/2.8b0e4c38.chunk.css"
+  },
+  {
+    "revision": "67ac3afee09748bb127840f57efcc25c",
+    "url": "/react-clock/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-clock/precache-manifest.9c9da2b45df12310e0d1d10bdf500264.js"
+  "/react-clock/precache-manifest.01d0cf0f3203817ab79fac82b7d457e2.js"
 );
 
 workbox.clientsClaim();
