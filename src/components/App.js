900f10539c3090d9d21bab189afb289a9d5ab96e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-13 22:07:05 +0200

    add algorithm of sorting and distributing countries to timezones in App.js instead of data.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment-timezone";
 import Clock from "./Clock";
-import times from "./data"
+import timezonesRaw from "./data"
 import "../styles/App.css";
 
 export default function App() {
+  const [timezones, setTimezones] = useState(null)
+  const prepareZones = () => {
+    const Now = moment().utc().format("x")
+    const unsortedStates = timezonesRaw.map(tz => {
+      const {country, zone, flag, subdiv } = tz
+      const offset = moment().tz(zone).format("Z")
+      const numericOffset = -1 * moment.tz.zone(zone).utcOffset(Now)
+      return { country, zone, flag, offset, numericOffset, subdiv }
+    })
+    const sortedStates = unsortedStates.sort((a,b) => (a.numericOffset - b.numericOffset))
+    const timezoneObject = sortedStates.reduce((acc, curr) => {
+      const obj = {...acc}
+      const { flag: code, offset, country: title, zone, numericOffset, subdiv = [] } = curr
+      const city = "UTC" + offset
+      const flag = { code, title, subdiv }
+      if (obj.hasOwnProperty(city)) {
+        obj[city].flags.push(flag)
+      } else {
+        obj[city] = { city, numericOffset, zone, flags: [flag] }
+      }
+      return obj
+    }, {})
+    const timezoneArray = Object.values(timezoneObject).sort((a,b) => (a.numericOffset - b.numericOffset))
+    setTimezones(timezoneArray)
+  }
+  useEffect(() => {
+    prepareZones()
+    setInterval(1000,prepareZones)
+  }, [])
   return (
     <div className="app">
       <h1>World Time Clock</h1>
       <div className="row album sk-album"> 
-      {times.map((time, index) => <Clock key={index} flags={time.flags} city={time.city} zone={time.zone} />)}
+      {timezones && timezones.length > 0 && timezones.map((time, index) => <Clock key={index} flags={time.flags} city={time.city} zone={time.zone} />)}
       </div>
     </div>
   );
--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,3 @@
-import moment from "moment-timezone"
 import Africa from "./timezones/Africa"
 import Australia from "./timezones/Australia"
 import Canada from "./timezones/Canada"
@@ -16,7 +15,7 @@ import SouthAsia from "./timezones/SouthAsia"
 import SouthEastAsia from "./timezones/SouthEastAsia"
 import UnitedStates from "./timezones/UnitedStates"
 
-const timezonesRaw = [
+export default [
   ...Africa, 
   ...Australia,
   ...Canada,
@@ -34,26 +33,3 @@ const timezonesRaw = [
   ...SouthEastAsia,
   ...UnitedStates
 ]
-
-const Now = moment().utc().format("x")
-const rawZones = timezonesRaw.map(tz => {
-  const {country, zone, flag, subdiv } = tz
-  const offset = moment().tz(zone).format("Z")
-  const numericOffset = -1 * moment.tz.zone(zone).utcOffset(Now)
-  return { country, zone, flag, offset, numericOffset, subdiv }
-}).sort((a, b) => (a.numericOffset - b.numericOffset))
-const timeZoneObject = rawZones.reduce((acc, curr) => {
-  const obj = {...acc}
-  const { flag: code, offset, country: title, zone, numericOffset, subdiv = [] } = curr
-  const city = "UTC" + offset
-  const flag = { code, title, subdiv }
-  if (obj.hasOwnProperty(city)) {
-    obj[city].flags.push(flag)
-  } else {
-    obj[city] = { city, numericOffset, zone, flags: [flag] }
-  }
-  return obj
-}, {})
-const timeZoneArray = Object.values(timeZoneObject).sort((a,b) => (a.numericOffset - b.numericOffset))
-
-export default timeZoneArray
