c43105afd78cfa1bc1843ca2735146c9f9561e33
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-13 21:23:31 +0200

    add United States

--- src/components/data.js
+++ src/components/data.js
@@ -14,6 +14,7 @@ import Russia from "./timezones/Russia"
 import SouthAmerica from "./timezones/SouthAmerica"
 import SouthAsia from "./timezones/SouthAsia"
 import SouthEastAsia from "./timezones/SouthEastAsia"
+import UnitedStates from "./timezones/UnitedStates"
 
 const timezonesRaw = [
   ...Africa, 
@@ -31,6 +32,7 @@ const timezonesRaw = [
   ...SouthAmerica,
   ...SouthAsia,
   ...SouthEastAsia,
+  ...UnitedStates
 ]
 
 const Now = moment().utc().format("x")
---
+++ src/components/timezones/UnitedStates.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    country: "United States",
+    subdiv: ["Alabama", "Arkansas", "Florida (west of Appalachicola River)", "Illinois", "Iowa", "Kansas (most of state)", "Kentucky (western 40%)", "Louisiana", "Minnesota", "Mississippi", "Missouri", "Nebraska (most of state)", "Oklahoma", "South Dakota (eastern half)", "Tennessee (most of state)", "Texas (most of state)", "Wisconsin"],
+    zone: "America/Chicago",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Alaska (west of 169°30' W)"],
+    zone: "America/Adak",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Alaska (east of 169°30' W)"],
+    zone: "America/Anchorage",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Arizona"],
+    zone: "America/Phoenix",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["California", "Nevada (most of state)", "Oregon (most of state)", "Washington"],
+    zone: "America/Los_Angeles",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Colorado", "Kansas (western border counties)", "Montana", "Nebraska (western third)", "Nevada (West Wendover)", "New Mexico", "North Dakota (southwestern part)", "Oregon (eastern border counties)", "South Dakota (western half)", "Texas (El Paso, Hudspeth)", "Utah", "Wyoming"],
+    zone: "America/Denver",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Connecticut", "Delaware", "District of Columbia", "Florida (east of Appalachicola River)", "Georgia", "Maine", "Maryland", "Massachusetts", "New Hampshire", "New Jersey", "New York", "North Carolina", "Ohio", "Pennsylvania", "Rhode Island", "South Carolina", "Tennessee (eastern part)", "Vermont", "Virginia", "West Virginia"],
+    zone: "America/New_York",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Idaho (most of state)", "Oregon (east of Salmon and Snake River)"],
+    zone: "America/Boise",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Indiana (most of state)"],
+    zone: "America/Indiana/Indianapolis",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Indiana (northwest and southwest)"],
+    zone: "America/Indiana/Knox",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Kentucky (eastern 60%)"],
+    zone: "America/Kentucky/Louisville",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Michigan (most of state)"],
+    zone: "America/Detroit",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["Michigan (counties bordering Wisconsin)"],
+    zone: "America/Menominee",
+    flag: "US"
+  },
+  {
+    country: "United States",
+    subdiv: ["North Dakota (most of state)"],
+    zone: "America/North_Dakota/Center",
+    flag: "US"
+  },
+]
