2b70da31961eed707be4639e1a5d402f9f63a693
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-13 17:54:39 +0200

    add Canada

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,7 @@
 import moment from "moment-timezone"
 import Africa from "./timezones/Africa"
 import Australia from "./timezones/Australia"
+import Canada from "./timezones/Canada"
 import Caribbean from "./timezones/Caribbean"
 import CentralAmerica from "./timezones/CentralAmerica"
 import CentralAsia from "./timezones/CentralAsia"
@@ -17,6 +18,7 @@ import SouthEastAsia from "./timezones/SouthEastAsia"
 const timezonesRaw = [
   ...Africa, 
   ...Australia,
+  ...Canada,
   ...Caribbean,
   ...CentralAmerica,
   ...CentralAsia, 
--- src/components/timezones/NorthAmerica.js
+++ src/components/timezones/NorthAmerica.js
@@ -25,39 +25,16 @@ export const Arizona = {
   zone: "America/Phoenix",
   flags: [
     {
-      title: "Canada: Yukon",
-      code: "CA"
-    },
-    {
       title: "United States: Arizona (excluding Navajo Nation Reservation)",
       code: "US"
     }
   ]
 }
-
-export const AtlanticOcean = {
-  city: "Atlantic Ocean",
-  zone: "Atlantic/Azores",
-  flags: [
-    {
-      title: "Greenland (Denmark): Ittoqqortoormit",
-      code: "GL"
-    },
-    {
-      title: "Portugal: Azores",
-      code: "PT"
-    },
-  ]
-}
-
+ 
 export const Central = {
   city: "Central",
   zone: "America/Chicago",
   flags: [
-    {
-      title: "Canada: Manitoba, Nunavut, Ontario",
-      code: "CA"
-    },
     {
       title: "United States: Alabama, Arkansas, Florida (west of Appalachicola River), Illinois, Indiana (northwest and southwest), Iowa, Kansas (most of state), Kentucky (western 40%), Louisiana, Michigan (upper peninsula bordering Wisconsin), Minnesota, Mississippi, Missouri, Nebraska (most of state), North Dakota (east of Missouri River), Oklahoma, South Dakota (eastern half), Tennessee (most of state), Texas (most of state), Wisconsin",
       code: "US"
@@ -65,25 +42,10 @@ export const Central = {
   ]
 }
 
-export const CoteNord = {
-  city: "Côte-Nord",
-  zone: "America/Blanc-Sablon",
-  flags: [
-    {
-      title: "Canada: Québec (east of 63° W)",
-      code: "CA"
-    },
-  ]
-}
-
 export const Eastern = {
   city: "Eastern",
   zone: "America/New_York",
   flags: [
-    {
-      title: "Canada: Nunavut, Ontario, Québec (west of 63° W)",
-      code: "CA"
-    },
     {
       title: "United States: Connecticut, Delaware, District of Columbia, Florida (east of Appalachicola River), Georgia, Indiana (most of state), Kentucky (eastern 60%), Maine, Maryland, Massachusetts, Michigan (most of state), New Hampshire, New Jersey, New York, North Carolina, Ohio, Pennsylvania, Rhode Island, South Carolina, Tennessee (east of state), Vermont, Virginia, West Virginia",
       code: "US"
@@ -96,38 +58,8 @@ export const Mountain = {
   zone: "America/Denver",
   flags: [
     {
-      title: "Canada: Alberta, British Columbia, Northwest Territories, Nunavut",
-      code: "CA"
-    },
-    {
       title: "United States: Arizona (only Navajo Nation Reservation), Colorado, Idaho (most of state), Kansas (western border), Montana, Nebraska (panhandle), Nevada (West Wendover), New Mexico, North Dakota (west of Missouri River), Oregon (eastern border), South Dakota (western half), Texas (El Paso, Hudspeth), Utah, Wyoming",
       code: "US"
-    }
-  ]
-}
-
-export const Newfoundland = {
-  city: "Newfoundland",
-  zone: "America/St_Johns",
-  flags: [
-    {
-      title: "Canada: Newfoundland",
-      code: "CA"
-    }
-  ]
-}
-
-export const NorthwesternAtlantic = {
-  city: "Northwestern Atlantic",
-  zone: "America/Halifax",
-  flags: [
-    {
-      title: "Canada: New Brunswick, Labrador, Nova Scotia, Prince Edward Island, Québec",
-      code: "CA"
-    },
-    {
-      title: "Greenland: Thule Air Base",
-      code: "GL"
     }
   ]
 }
@@ -137,34 +69,8 @@ export const Pacific = {
   zone: "America/Los_Angeles",
   flags: [
     {
-      title: "Canada: British Columbia, Northwest Territories",
-      code: "CA"
-    },
-    {
       title: "United States: California, Idaho (panhandle), Nevada (most of state), Oregon (most of state), Washington",
       code: "US"
     }
-  ]
-}
-
-export const Saskatchewan = {
-  city: "Saskatchewan",
-  zone: "America/Regina",
-  flags: [
-    {
-      title: "Canada: Saskatchewan",
-      code: "CA"
-    }
-  ]
-}
-
-export const WesternGreenland = {
-  city: "Western Greenland",
-  zone: "America/Godthab",
-  flags: [
-    {
-      title: "Greenland (Denmark): Western Greenland",
-      code: "GL",
-    },
   ]
 }
