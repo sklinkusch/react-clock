ccabb6c243920e4ec8117ca822841d31b9507213
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-14 16:54:07 +0200

    add input element

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,22 @@ import timezonesRaw from "./data"
 import "../styles/App.css";
 
 export default function App() {
+  const [allStates] = useState(timezonesRaw)
   const [timezones, setTimezones] = useState(null)
+  const [filterValue, setFilterValue] = useState("")
   const prepareZones = () => {
     const Now = moment().utc().format("x")
-    const unsortedStates = timezonesRaw.map(tz => {
+    const filteredStates = allStates.filter(tz => {
+      const { country } = tz
+      if(filterValue.length > 0){
+        if(country.toLowerCase().includes(filterValue.toLowerCase())) {
+          return true
+        }
+        return false
+      }
+      return true
+    })
+    const unsortedStates = filteredStates.map(tz => {
       const {country, zone, flag, subdiv } = tz
       const offset = moment().tz(zone).format("Z")
       const numericOffset = -1 * moment.tz.zone(zone).utcOffset(Now)
@@ -37,6 +49,9 @@ export default function App() {
   return (
     <div className="app">
       <h1>World Time Clock</h1>
+      <div style={{ textAlign: "center", marginBottom: "24px" }}>
+        <input type="text" placeholder="Filter countries" onChange={(e) => setFilterValue(e.target.value)} />
+      </div>
       <div className="row album sk-album"> 
       {timezones && timezones.length > 0 && timezones.map((time, index) => <Clock key={index} flags={time.flags} city={time.city} zone={time.zone} />)}
       </div>
