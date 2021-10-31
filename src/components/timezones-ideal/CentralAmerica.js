3ab990619e77b03a0baacba33b5c9c3c17291628
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-10-31 21:20:34 +0100

    add ideal timezones for Central America

--- src/components/timezones-ideal/CentralAmerica.js
+++ src/components/timezones-ideal/CentralAmerica.js
@@ -1,35 +1,44 @@
 export default [
   {
     country: "Belize",
-    zone: "America/Belize",
+    zone: "Etc/GMT+7",
     flag: "BZ"
   },
   {
     country: "Costa Rica",
-    zone: "America/Costa_Rica",
+    zone: "Etc/GMT+6",
     flag: "CR"
   },
   {
     country: "El Salvador",
-    zone: "America/El_Salvador",
+    zone: "Etc/GMT+6",
     flag: "SV"
   },
   {
     country: "Guatemala",
-    zone: "America/Guatemala",
+    zone: "Etc/GMT+6",
     flag: "GT"
   },
   {
     country: "Honduras",
-    zone: "America/Tegucigalpa",
+    zone: "Etc/GMT+6",
     flag: "HN"
   },
   {
     country: "Mexico",
     subdiv: [
+      {title: "Baja California"},
+      {title: "Baja California Sur"},
+      {title: "Revillagigedo Islands"}
+    ],
+    zone: "Etc/GMT+8",
+    flag: "MX"
+  },
+  {
+    country: "Mexico",
+    subdiv: [
       {title: "Aguascalientes"}, 
-      {title: "Campeche"}, 
-      {title: "Chiapas"}, 
+      {title: "Chihuahua"},
       {title: "Coahuila"}, 
       {title: "Colima"}, 
       {title: "Distrito Federal"}, 
@@ -41,66 +50,42 @@ export default [
       {title: "México"}, 
       {title: "Michoacán"}, 
       {title: "Morelos"}, 
+      {title: "Nayarit"},
       {title: "Nuevo León"}, 
-      {title: "Oaxaca"}, 
       {title: "Puebla"}, 
       {title: "Querétaro"}, 
       {title: "San Luis Potosí"}, 
-      {title: "Tabasco"}, 
+      {title: "Sinaloa"},
+      {title: "Sonora"},
       {title: "Tamaulipas"}, 
       {title: "Tlaxcala"}, 
-      {title: "Veracruz"}, 
-      {title: "Yucatán"}, 
       {title: "Zacatecas"}
     ],
-    zone: "America/Mexico_City",
+    zone: "Etc/GMT+7",
     flag: "MX"
   },
   {
     country: "Mexico",
     subdiv: [
-      {title: "Baja California"}
+      {title: "Campeche"}, 
+      {title: "Chiapas"}, 
+      {title: "Oaxaca"}, 
+      {title: "Quintana Roo"},
+      {title: "Tabasco"}, 
+      {title: "Veracruz"}, 
+      {title: "Yucatán"}, 
     ],
-    zone: "America/Tijuana",
-    flag: "MX"
-  },
-  {
-    country: "Mexico",
-    subdiv: [
-      {title: "Baja California Sur"},
-      {title: "Chihuahua"},
-      {title: "Nayarit"},
-      {title: "Sinaloa"}
-    ]
-    ,
-    zone: "America/Mazatlan",
-    flag: "MX"
-  },
-  {
-    country: "Mexico",
-    subdiv: [
-      {title: "Quintana Roo"}
-    ],
-    zone: "America/Cancun",
-    flag: "MX"
-  },
-  {
-    country: "Mexico",
-    subdiv: [
-      {title: "Sonora"},
-      {title: "Revillagigedo Islands"}
-    ],
-    zone: "America/Hermosillo",
+    zone: "Etc/GMT+6",
     flag: "MX"
   },
   {
     country: "Nicaragua",
-    zone: "America/Managua",
+    zone: "Etc/GMT+6",
     flag: "NI"
   },
   {
     country: "Panama",
-    zone: "America/Panama",
+    zone: "Etc/GMT+5",
     flag: "PA"
   },
 ]
