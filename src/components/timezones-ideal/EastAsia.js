ac630ad76850dbd2b1cb8012701b2b3fe15a58e0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-10-31 22:39:43 +0100

    add ideal timezones for East Asia

--- src/components/timezones-ideal/EastAsia.js
+++ src/components/timezones-ideal/EastAsia.js
@@ -1,31 +1,106 @@
 export default [
   {
     country: "China",
-    zone: "Asia/Shanghai",
+    subdiv: [
+      {title: "Tibet", extra: "(west of 82.5°E)"},
+      {title: "Xinjiang", extra: "(west of 82.5°E)"},
+    ],
+    zone: "Etc/GMT-5",
     flag: "CN"
   },
   {
     country: "China",
     subdiv: [
-      {title: "Xinjiang"}
+      {title: "Gansu", extra: "(west of 97.5°E)"},
+      {title: "Qinghai", extra: "(west of 97.5°E)"},
+      {title: "Tibet", extra: "(east of 82.5°E)"},
+      {title: "Xinjiang", extra: "(east of 82.5°E)"},
     ],
-    zone: "Asia/Urumqi",
+    zone: "Etc/GMT-6",
+    flag: "CN"
+  },
+  {
+    country: "China",
+    subdiv: [
+      {title: "Chongqing"},
+      {title: "Gansu", extra: "(east of 97.5°E)"},
+      {title: "Guangdong", extra: "(west of 112.5°E)"},
+      {title: "Guangxi Zhuang"},
+      {title: "Guizhou"},
+      {title: "Hainan"},
+      {title: "Hubei", extra: "(west of 112.5°E)"},
+      {title: "Hunan"},
+      {title: "Inner Mongolia", extra: "(west of 112.5°E)"},
+      {title: "Ningxia"},
+      {title: "Qinghai", extra: "(east of 97.5°E)"},
+      {title: "Shaanxi"},
+      {title: "Shanxi"},
+      {title: "Sichuan"},
+      {title: "Yunnan"},
+    ],
+    zone: "Etc/GMT-7",
+    flag: "CN"
+  },
+  {
+    country: "China",
+    subdiv: [
+      {title: "Anhui"},
+      {title: "Beijing"},
+      {title: "Fujian"},
+      {title: "Guangdong", extra: "(east of 112.5°E)"},
+      {title: "Hebei"},
+      {title: "Henan"},
+      {title: "Hubei", extra: "(east of 112.5°E)"},
+      {title: "Heilongjiang", extra: "(west of 127.5°E)"},
+      {title: "Inner Mongolia", extra: "(east of 112.5°E)"},
+      {title: "Jiangsu"},
+      {title: "Jiangxi"},
+      {title: "Jilin", extra: "(west of 127.5°E)"},
+      {title: "Liaoning"},
+      {title: "Nanchang"},
+      {title: "Shandong"},
+      {title: "Shanghai"},
+      {title: "Tianjin"},
+      {title: "Zhejiang"},
+    ],
+    zone: "Etc/GMT-8",
+    flag: "CN"
+  },
+  {
+    country: "China",
+    subdiv: [
+      {title: "Heilongjiang", extra: "(east of 127.5°E)"},
+      {title: "Jilin", extra: "(east of 127.5°E)"},
+    ],
+    zone: "Etc/GMT-9",
     flag: "CN"
   },
   {
     country: "Hong Kong (China)",
-    zone: "Asia/Hong_Kong",
+    zone: "Etc/GMT-8",
     flag: "HK"
   },
   {
     country: "Japan",
-    zone: "Asia/Tokyo",
+    zone: "Etc/GMT-9",
     flag: "JP"
   },
   {
     country: "Macau (China)",
-    zone: "Asia/Macau",
+    zone: "Etc/GMT-8",
     flag: "MO"
+  },
+  {
+    country: "Mongolia",
+    subdiv: [
+      {title: "Bayan-Ölgii"}, 
+      {title: "Govi-Altai"}, 
+      {title: "Khovd"}, 
+      {title: "Uvs"}, 
+      {title: "Zavkhan"}
+    ],
+    zone: "Etc/GMT-6",
+    flag: "MN"
   },
   {
     country: "Mongolia",
@@ -45,7 +120,7 @@ export default [
       {title: "Ömnögovi"}, 
       {title: "Övörkhanghai"}
     ],
-    zone: "Asia/Ulaanbaatar",
+    zone: "Etc/GMT-7",
     flag: "MN"
   },
   {
@@ -54,34 +129,22 @@ export default [
       {title: "Dornod"}, 
       {title: "Sükhbaatar"}
     ],
-    zone: "Asia/Choibalsan",
-    flag: "MN"
-  },
-  {
-    country: "Mongolia",
-    subdiv: [
-      {title: "Bayan-Ölgii"}, 
-      {title: "Govi-Altai"}, 
-      {title: "Khovd"}, 
-      {title: "Uvs"}, 
-      {title: "Zavkhan"}
-    ],
-    zone: "Asia/Hovd",
+    zone: "Etc/GMT-8",
     flag: "MN"
   },
   {
     country: "North Korea",
-    zone: "Asia/Pyongyang",
+    zone: "Etc/GMT-9",
     flag: "KP"
   },
   {
     country: "South Korea",
-    zone: "Asia/Seoul",
+    zone: "Etc/GMT-9",
     flag: "KR"
   },
   {
     country: "Taiwan",
-    zone: "Asia/Taipei",
+    zone: "Etc/GMT-8",
     flag: "TW"
   }
 ]
