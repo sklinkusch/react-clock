68ecfaf6bacecde737c5960212101c066e6dd56d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-03 20:34:25 +0100

    add ideal timezones for South Asia

--- src/components/timezones-ideal/SouthAsia.js
+++ src/components/timezones-ideal/SouthAsia.js
@@ -1,52 +1,113 @@
 export default [
   {
     country: "Bangladesh",
-    zone: "Asia/Dhaka",
+    zone: "Etc/GMT-6",
     flag: "BD"
   },
   {
     country: "Bhutan",
-    zone: "Asia/Thimphu",
+    zone: "Etc/GMT-6",
     flag: "BT"
   },
   {
     country: "British Indian Ocean Territory (United Kingdom)",
-    zone: "Indian/Chagos",
+    zone: "Etc/GMT-5",
     flag: "IO"
   },
   {
     country: "Cocos Islands (Australia)",
-    zone: "Indian/Cocos",
+    zone: "Etc/GMT-6",
     flag: "CC"
   },
   {
     country: "India",
-    zone: "Asia/Kolkata",
+    subdiv: [
+      {title: "Andhra Pradesh"},
+      {title: "Chhattisgarh"},
+      {title: "Goa"},
+      {title: "Gujarat"},
+      {title: "Haryana"},
+      {title: "Himachal Pradesh"},
+      {title: "Karnataka"},
+      {title: "Kerala"},
+      {title: "Madhya Pradesh"},
+      {title: "Maharashtra"},
+      {title: "Punjab"},
+      {title: "Rajasthan"},
+      {title: "Tamil Nadu"},
+      {title: "Telangana"},
+      {title: "Uttarakhand"},
+      {title: "Uttar Pradesh"},
+      {title: "Dadra and Nagar Haveli and Daman and Du"},
+      {title: "Delhi"},
+      {title: "Jammu and Kashmir"},
+      {title: "Ladakh"},
+      {title: "Lakshadweep"},
+      {title: "Puducherry"},
+    ],
+    zone: "Etc/GMT-5",
+    flag: "IN"
+  },
+  {
+    country: "India",
+    subdiv: [
+      {title: "Arunachal Pradesh"},
+      {title: "Assam"},
+      {title: "Bihar"},
+      {title: "Jharkhand"},
+      {title: "Manipur"},
+      {title: "Meghalaya"},
+      {title: "Mizoram"},
+      {title: "Nagaland"},
+      {title: "Odisha"},
+      {title: "Sikkim"},
+      {title: "Tripura"},
+      {title: "West Bengal"},
+      {title: "Andaman and Nicobar Islands"},
+      {title: "Chandigarh"},
+    ],
+    zone: "Etc/GMT-6",
     flag: "IN"
   },
   {
     country: "Myanmar",
-    zone: "Asia/Yangon",
+    utcOffset: 390,
     flag: "MM"
   },
   {
     country: "Nepal",
-    zone: "Asia/Kathmandu",
+    zone: "Etc/GMT-5",
     flag: "NP"
   },
   {
     country: "Maldives",
-    zone: "Indian/Maldives",
+    zone: "Etc/GMT-5",
     flag: "MV"
   },
   {
     country: "Pakistan",
-    zone: "Asia/Karachi",
+    subdiv: [
+      {title: "Balochistan"},
+    ],
+    zone: "Etc/GMT-4",
+    flag: "PK"
+  },
+  {
+    country: "Pakistan",
+    subdiv: [
+      {title: "Azad Jammu and Kashmir"},
+      {title: "Gilgit-Baltistan"},
+      {title: "Islamabad Capital Territory"},
+      {title: "Khyber Pakhtunkhwa"},
+      {title: "Punjab"},
+      {title: "Sindh"},
+    ],
+    zone: "Etc/GMT-5",
     flag: "PK"
   },
   {
     country: "Sri Lanka",
-    zone: "Asia/Colombo",
+    zone: "Etc/GMT-5",
     flag: "LK"
   }
 ]
