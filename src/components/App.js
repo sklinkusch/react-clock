c5ec813568dfb7a01fdddf06a1755bdf15b9fb45
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-04 20:07:15 +0100

    switch BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header"
 import Home from "../pages/Home";
 import Ideal from "../pages/Ideal";
