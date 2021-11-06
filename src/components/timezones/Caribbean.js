db756e845774f3acd8ff9e42e8220d5a5a1aa367
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-06 23:46:16 +0100

    add real cities for the Caribbean

--- src/components/timezones/Caribbean.js
+++ src/components/timezones/Caribbean.js
@@ -1,68 +1,83 @@
+import cityList from "./cityList"
+
 export default [
   {
     country: "American Virgin Islands (United States)",
     zone: "America/St_Thomas",
-    flag: "VI"
+    flag: "VI",
+    citites: cityList.filter(city => city.country === "VI" && city.tz === "America/St_Thomas")
   },
   {
     country: "Anguilla (United Kingdom)",
     zone: "America/Anguilla",
-    flag: "AI"
+    flag: "AI",
+    citites: cityList.filter(city => city.country === "AI" && city.tz === "America/Anguilla")
   },
   {
     country: "Antigua and Barbuda",
     zone: "America/Antigua",
-    flag: "AG"
+    flag: "AG",
+    citites: cityList.filter(city => city.country === "AG" && city.tz === "America/Antigua")
   },
   {
     country: "Aruba (Netherlands)",
     zone: "America/Aruba",
-    flag: "AW"
+    flag: "AW",
+    citites: cityList.filter(city => city.country === "AW" && city.tz === "America/Aruba")
   },
   {
     country: "Bahamas",
     zone: "America/Nassau",
-    flag: "BS"
+    flag: "BS",
+    citites: cityList.filter(city => city.country === "BS" && city.tz === "America/Nassau")
   },
   {
     country: "Barbados",
     zone: "America/Barbados",
-    flag: "BB"
+    flag: "BB",
+    citites: cityList.filter(city => city.country === "BB" && city.tz === "America/Barbados")
   },
   {
     country: "Bermuda (United Kingdom)",
     zone: "Atlantic/Bermuda",
-    flag: "BM"
+    flag: "BM",
+    citites: cityList.filter(city => city.country === "BM" && city.tz === "America/Bermuda")
   },
   {
     country: "British Virgin Islands (United Kingdom)",
     zone: "America/Tortola",
-    flag: "VG"
+    flag: "VG",
+    citites: cityList.filter(city => city.country === "VG" && city.tz === "America/Tortola")
   },
   {
     country: "Cayman Islands (United Kingdom)",
     zone: "America/Cayman",
-    flag: "KY"
+    flag: "KY",
+    citites: cityList.filter(city => city.country === "KY" && city.tz === "America/Cayman")
   },
   {
     country: "Cuba",
     zone: "America/Havana",
-    flag: "CU"
+    flag: "CU",
+    citites: cityList.filter(city => city.country === "CU" && city.tz === "America/Havana")
   },
   {
     country: "Curaçao (Netherlands)",
     zone: "America/Curacao",
     flag: "CW",
+    citites: cityList.filter(city => city.country === "CW" && city.tz === "America/Curacao")
   },
   {
     country: "Dominica",
     zone: "America/Dominica",
-    flag: "DM"
+    flag: "DM",
+    citites: cityList.filter(city => city.country === "DM" && city.tz === "America/Dominica")
   },
   {
     country: "Dominican Republic",
     zone: "America/Santo_Domingo",
-    flag: "DO"
+    flag: "DO",
+    citites: cityList.filter(city => city.country === "DO" && city.tz === "America/Santo_Domingo")
   },
   {
     country: "France",
@@ -70,7 +85,8 @@ export default [
       {title: "Guadeloupe"}
     ],
     zone: "America/Guadeloupe",
-    flag: "FR" // eigentlich GP
+    flag: "FR", // eigentlich GP
+    citites: cityList.filter(city => city.country === "GP" && city.tz === "America/Guadeloupe")
   },
   {
     country: "France",
@@ -78,12 +94,14 @@ export default [
       {title: "Saint-Martin"}
     ],
     zone: "America/Marigot",
-    flag: "FR" // eigentlich MF
+    flag: "FR", // eigentlich MF
+    citites: cityList.filter(city => city.country === "MF" && city.tz === "America/Marigot")
   },
   {
     country: "Saint-Barthélemy (France)",
     zone: "America/St_Barthelemy",
-    flag: "BL"
+    flag: "BL",
+    citites: cityList.filter(city => city.country === "BL" && city.tz === "America/St_Barthelemy")
   },
   {
     country: "France",
@@ -91,32 +109,38 @@ export default [
       {title: "Saint-Pierre and Miquelon"}
     ],
     zone: "America/Miquelon",
-    flag: "FR" // eigentlich DF
+    flag: "FR", // eigentlich DF
+    citites: cityList.filter(city => city.country === "DF" && city.tz === "America/Miquelon")
   },
   {
     country: "Grenada",
     zone: "America/Grenada",
-    flag: "GD"
+    flag: "GD",
+    citites: cityList.filter(city => city.country === "GD" && city.tz === "America/Grenada")
   },
   {
     country: "Haiti",
     zone: "America/Port-au-Prince",
-    flag: "HT"
+    flag: "HT",
+    citites: cityList.filter(city => city.country === "HT" && city.tz === "America/Port-au-Prince")
   },
   {
     country: "Jamaica",
     zone: "America/Jamaica",
-    flag: "JM"
+    flag: "JM",
+    citites: cityList.filter(city => city.country === "JM" && city.tz === "America/Jamaica")
   },
   {
     country: "Martinique (France)",
     zone: "America/Martinique",
-    flag: "MQ"
+    flag: "MQ",
+    citites: cityList.filter(city => city.country === "MQ" && city.tz === "America/Martinique")
   },
   {
     country: "Montserrat (United Kingdom)",
     zone: "America/Montserrat",
-    flag: "MS"
+    flag: "MS",
+    citites: cityList.filter(city => city.country === "MS" && city.tz === "America/Montserrat")
   },
   {
     country: "Netherlands",
@@ -126,41 +150,49 @@ export default [
       {title: "Sint Eustatius"}
     ],
     zone: "America/Kralendijk",
-    flag: "NL" // eigentlich BQ
+    flag: "NL", // eigentlich BQ
+    citites: cityList.filter(city => city.country === "BQ" && city.tz === "America/Kralendijk")
   },
   {
     country: "Puerto Rico (United States)",
     zone: "America/Puerto_Rico",
-    flag: "PR"
+    flag: "PR",
+    citites: cityList.filter(city => city.country === "PR" && city.tz === "America/Puerto_Rico")
   },
   {
     country: "Saint Kitts and Nevis",
     zone: "America/St_Kitts",
-    flag: "KN"
+    flag: "KN",
+    citites: cityList.filter(city => city.country === "KN" && city.tz === "America/St_Kitts")
   },
   {
     country: "Saint Lucia",
     zone: "America/St_Lucia",
-    flag: "LC"
+    flag: "LC",
+    citites: cityList.filter(city => city.country === "LC" && city.tz === "America/St_Lucia")
   },
   {
     country: "Saint Vincent and the Grenadines",
     zone: "America/St_Vincent",
-    flag: "VC"
+    flag: "VC",
+    citites: cityList.filter(city => city.country === "VC" && city.tz === "America/St_Vincent")
   },
   {
     country: "Sint Maarten (Netherlands)",
     zone: "America/Lower_Princes",
-    flag: "SX"
+    flag: "SX",
+    citites: cityList.filter(city => city.country === "SX" && city.tz === "America/Lower_Princes")
   },
   {
     country: "Trinidad and Tobago",
     zone: "America/Port_of_Spain",
-    flag: "TT"
+    flag: "TT",
+    citites: cityList.filter(city => city.country === "TT" && city.tz === "America/Port_of_Spain")
   },
   {
     country: "Turks and Caicos Islands (United Kingdom)",
     zone: "America/Grand_Turk",
-    flag: "TC"
+    flag: "TC",
+    citites: cityList.filter(city => city.country === "TC" && city.tz === "America/Grand_Turk")
   },
 ]
