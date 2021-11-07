945933ae7cc24c82694159c00835c24ac7e083b9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-07 21:21:25 +0100

    add ideal cities of Central Asia

--- src/components/timezones-ideal/CentralAsia.js
+++ src/components/timezones-ideal/CentralAsia.js
@@ -1,56 +1,102 @@
+import cityList from "./cityList"
+
 export default [
   {
     country: "Kazakhstan",
     subdiv: [
-      {title: "Atyrau Region"},
-      {title: "Mangystau Region"},
-      {title: "West Kazakhstan Region"}
+      {title: "Atyrau Region"}, // 06
+      {title: "Mangystau Region"}, // 09
+      {title: "West Kazakhstan Region"} // 07
     ],
-    zone: "Etc/GMT-3"
+    zone: "Etc/GMT-3",
+    cities : cityList.filter(city => {
+      if(city.country === "KZ") {
+        if(city.adminCode === "06") return true
+        if(city.adminCode === "07") return true
+        if(city.adminCode === "09") return true
+        return false
+      }
+      return false
+    })
   },
   {
     country: "Kazakhstan",
     subdiv: [
-      {title: "Aktobe Region"},
-      {title: "Kostanay Region"},
-      {title: "Kyzylorda Region"}
+      {title: "Aktobe Region"}, // 04
+      {title: "Kostanay Region"}, // 13
+      {title: "Kyzylorda Region"}, // 14
+      {title: "Baykonyr"} // 18
     ],
     zone: "Etc/GMT-4",
-    flag: "KZ"
+    flag: "KZ",
+    cities: cityList.filter(city => {
+      if(city.country === "KZ") {
+        if(city.adminCode === "04") return true
+        if(city.adminCode === "13") return true
+        if(city.adminCode === "14") return true
+        if(city.adminCode === "18") return true
+        return false
+      }
+      return false
+    })
   },
   {
     country: "Kazakhstan",
     subdiv: [
-      {title: "Akmola Region"}, 
-      {title: "Almaty Region"}, 
-      {title: "East Kazakhstan Region"}, 
-      {title: "Jambyl Region"}, 
-      {title: "Karaganda Region"}, 
-      {title: "North Kazakhstan Region"}, 
-      {title: "Pavlodar Region"}, 
-      {title: "Turkistan Region"}
+      {title: "Akmola Region"}, // 03
+      {title: "Almaty Region"}, // 01
+      {title: "Almaty"}, // 02
+      {title: "Nur-Sultan"}, // 05
+      {title: "East Kazakhstan Region"}, // 15
+      {title: "Jambyl Region"}, // 17
+      {title: "Karaganda Region"}, // 12
+      {title: "North Kazakhstan Region"}, // 16 
+      {title: "Pavlodar Region"}, // 11
+      {title: "Turkistan Region"}, // 10
+      {title: "Shymkent"} // 1537272
     ],
     zone: "Etc/GMT-5",
-    flag: "KZ"
+    flag: "KZ",
+    cities: cityList.filter(city => {
+      if(city.country === "KZ") {
+        if(city.adminCode === "01") return true
+        if(city.adminCode === "02") return true
+        if(city.adminCode === "03") return true
+        if(city.adminCode === "05") return true
+        if(city.adminCode === "10") return true
+        if(city.adminCode === "11") return true
+        if(city.adminCode === "12") return true
+        if(city.adminCode === "15") return true
+        if(city.adminCode === "16") return true
+        if(city.adminCode === "17") return true
+        if(city.adminCode === "1537272") return true
+        return false
+      }
+      return false
+    })
   },
   {
     country: "Kyrgyzstan",
     zone: "Etc/GMT-5",
-    flag: "KG"
+    flag: "KG",
+    cities: cityList.filter(city => city.country === "KG")
   },
   {
     country: "Tajikistan",
     zone: "Etc/GMT-5",
-    flag: "TJ"
+    flag: "TJ",
+    cities: cityList.filter(city => city.country === "TJ")
   },
   {
     country: "Turkmenistan",
     zone: "Etc/GMT-4",
-    flag: "TM"
+    flag: "TM",
+    cities: cityList.filter(city => city.country === "TM")
   },
   {
     country: "Uzbekistan",
     zone: "Etc/GMT-4",
-    flag: "UZ"
+    flag: "UZ",
+    cities: cityList.filter(city => city.country === "UZ")
   },
 ]
