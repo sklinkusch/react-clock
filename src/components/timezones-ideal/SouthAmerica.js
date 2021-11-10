39768d9250d67fc153218d7e60443fb23e40da6a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-10 18:37:36 +0100

    add ideal cities of South America

--- src/components/timezones-ideal/SouthAmerica.js
+++ src/components/timezones-ideal/SouthAmerica.js
@@ -1,72 +1,154 @@
+import cityList from "./cityList"
+
 export default [
   {
     country: "Argentina",
     zone: "Etc/GMT+4",
-    flag: "AR"
+    flag: "AR",
+    cities: cityList.filter(city => city.country === "AR")
   },
   {
     country: "Bolivia",
     zone: "Etc/GMT+4",
-    flag: "BO"
+    flag: "BO",
+    cities: cityList.filter(city => city.country === "BO")
   },
   {
     country: "Brazil",
     subdiv: [
-      {title: "Acre"},
-      {title: "Amazonas", extra: "(western part)"}
+      {title: "Acre"}, // 01
+      {title: "Amazonas", extra: "(western part)"} // 04
     ],
     zone: "Etc/GMT+5",
-    flag: "BR"
+    flag: "BR",
+    cities: cityList.filter(city => {
+      if(city.country === "BR") {
+        switch(city.adminCode) {
+          case "01":
+            return true
+          case "04":
+            if(city.lon < -67.5) return true
+            return false
+          default:
+            return false
+        }
+      }
+      return false
+    })
   },
   {
     country: "Brazil",
     subdiv: [
-      {title: "Amazonas", extra: "(eastern part)"},
-      {title: "Mato Grosso"},
-      {title: "Mato Grosso do Sul"},
-      {title: "Pará", extra: "(west of 52.5° W)"},
-      {title: "Rio Grande do Sul"}, 
-      {title: "Rondônia"},
-      {title: "Roraima"},
+      {title: "Amazonas", extra: "(eastern part)"}, // 04
+      {title: "Mato Grosso"}, // 14
+      {title: "Mato Grosso do Sul"}, // 11
+      {title: "Pará", extra: "(west of 52.5° W)"}, // 16
+      {title: "Rio Grande do Sul"}, // 23
+      {title: "Rondônia"}, // 24
+      {title: "Roraima"}, // 25
     ],
     zone: "Etc/GMT+4",
-    flag: "BR"
+    flag: "BR",
+    cities: cityList.filter(city => {
+      if(city.country === "BR") {
+        switch(city.adminCode) {
+          case "11":
+          case "14":
+          case "23":
+          case "24":
+          case "25":
+            return true
+          case "04":
+            if(city.lon >= -67.5) return true
+            return false
+          case "16":
+            if(city.lon < -52.5) return true
+            return false
+          default:
+            return false
+        }
+      }
+      return false 
+    })
   },
   {
     country: "Brazil",
     subdiv: [
-      {title: "Amapá"},
-      {title: "Bahia"},
-      {title: "Ceará"}, 
-      {title: "Distrito Federal"}, 
-      {title: "Espírito Santo"}, 
-      {title: "Goiás"}, 
-      {title: "Maranhão"}, 
-      {title: "Minas Gerais"}, 
-      {title: "Pará", extra: "(east of 52.5° W)"},
-      {title: "Paraná"}, 
-      {title: "Pernambuco"},
-      {title: "Piauí"}, 
-      {title: "Rio de Janeiro"}, 
-      {title: "Santa Catarina"}, 
-      {title: "São Paulo"},
-      {title: "Tocantins"},
+      {title: "Amapá"}, // 03
+      {title: "Bahia"}, // 05
+      {title: "Ceará"},  // 06
+      {title: "Distrito Federal"}, // 07
+      {title: "Espírito Santo"},  // 08
+      {title: "Goiás"}, // 29 
+      {title: "Maranhão"}, // 13
+      {title: "Minas Gerais"}, // 15
+      {title: "Pará", extra: "(east of 52.5° W)"}, // 16
+      {title: "Paraná"}, // 18
+      {title: "Pernambuco"}, // 30
+      {title: "Piauí"}, // 20
+      {title: "Rio de Janeiro"}, // 21
+      {title: "Santa Catarina"}, // 26
+      {title: "São Paulo"}, // 27
+      {title: "Tocantins"}, // 31
     ],
     zone: "Etc/GMT+3",
-    flag: "BR"
+    flag: "BR",
+    cities: cityList.filter(city => {
+      if(city.country === "BR") {
+        switch(city.adminCode) {
+          case "03":
+          case "05":
+          case "06":
+          case "07":
+          case "08":
+          case "13":
+          case "15":
+          case "18":
+          case "20":
+          case "21":
+          case "26":
+          case "27":
+          case "29":
+          case "30":
+          case "31":
+            return true
+          case "16":
+            if(city.lon >= -52.5) return true
+            return false
+          default:
+            return false
+        }
+      }
+      return false
+    })
   },
   {
     country: "Brazil",
     subdiv: [
-      {title: "Alagoas"}, 
+      {title: "Alagoas"}, // 02
       {title: "Fernando de Noronha"}, 
-      {title: "Paraíba"}, 
-      {title: "Rio Grande do Norte"},
-      {title: "Sergipe"},
+      {title: "Paraíba"}, // 17
+      {title: "Rio Grande do Norte"}, // 22
+      {title: "Sergipe"}, // 28
       {title: "Trinidade"}, 
     ],
     zone: "Etc/GMT+2",
-    flag: "BR"
+    flag: "BR",
+    cities: cityList.filter(city => {
+      if(city.country === "BR") {
+        if(city.tz === "America/Noronha") return true
+        switch(city.adminCode) {
+          case "02":
+          case "17":
+          case "22":
+          case "28":
+            return true
+          default:
+            return false
+        }
+      }
+      return false
+    })
   },
   {
     country: "Chile",
@@ -74,7 +156,8 @@ export default [
       {title: "Easter Island"}
     ],
     zone: "Etc/GMT+7",
-    flag: "CL"
+    flag: "CL",
+    cities: cityList.filter(city => city.country === "CL" && city.tz === "Pacific/Easter")
   },
   {
     country: "Chile",
@@ -97,12 +180,14 @@ export default [
       {title: "Valparaíso", extra: "(continental part)"},
     ],
     zone: "Etc/GMT+5",
-    flag: "CL"
+    flag: "CL",
+    cities: cityList.filter(city => city.country === "CL" && city.tz !== "Pacific/Easter")
   },
   {
     country: "Colombia",
     zone: "Etc/GMT+5",
-    flag: "CO"
+    flag: "CO",
+    cities: cityList.filter(city => city.country === "CO")
   },
   {
     country: "Ecuador",
@@ -132,7 +217,8 @@ export default [
       {title: "Zamora Chinchipe"}
     ],
     zone: "Etc/GMT+5",
-    flag: "EC"
+    flag: "EC",
+    cities: cityList.filter(city => city.country === "CL" && city.tz !== "Pacific/Galapagos")
   },
   {
     country: "Ecuador",
@@ -140,12 +226,14 @@ export default [
       {title: "Galápagos"}
     ],
     zone: "Etc/GMT+6",
-    flag: "EC"
+    flag: "EC",
+    cities: cityList.filter(city => city.country === "CL" && city.tz === "Pacific/Galapagos")
   },
   {
     country: "Falkland Islands (United Kingdom)",
     zone: "Etc/GMT+4",
-    flag: "FK"
+    flag: "FK",
+    cities: cityList.filter(city => city.country === "FK")
   },
   {
     country: "France",
@@ -153,41 +241,49 @@ export default [
       {title: "French Guiana"}
     ],
     zone: "Etc/GMT+4",
-    flag: "FR" // eigentlich GF
+    flag: "FR", // eigentlich GF
+    cities: cityList.filter(city => city.country === "GF")
   },
   {
     country: "Guyana",
     zone: "Etc/GMT+4",
-    flag: "GY"
+    flag: "GY",
+    cities: cityList.filter(city => city.country === "GY")
   },
   {
     country: "Paraguay",
     zone: "Etc/GMT+4",
-    flag: "PY"
+    flag: "PY",
+    cities: cityList.filter(city => city.country === "PY")
   },
   {
     country: "Peru",
     zone: "Etc/GMT+5",
-    flag: "PE"
+    flag: "PE",
+    cities: cityList.filter(city => city.country === "PE")
   },
   {
     country: "South Georgia and the South Sandwich Islands (United Kingdom)",
     zone: "Etc/GMT+2",
-    flag: "GS"
+    flag: "GS",
+    cities: cityList.filter(city => city.country === "GS")
   },
   {
     country: "Suriname",
     zone: "Etc/GMT+4",
-    flag: "SR"
+    flag: "SR",
+    cities: cityList.filter(city => city.country === "SR")
   },
   {
     country: "Uruguay",
     zone: "Etc/GMT+4",
-    flag: "UY"
+    flag: "UY",
+    cities: cityList.filter(city => city.country === "UY")
   },
   {
     country: "Venezuela",
     zone: "Etc/GMT+4",
-    flag: "VE"
+    flag: "VE",
+    cities: cityList.filter(city => city.country === "VE")
   }
 ]
