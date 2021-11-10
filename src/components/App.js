5eb3b93d0ea656cc991c4549f1fc1fa6ac5766f9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-10 22:55:03 +0100

    add code-splitting in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header"
-import Home from "../pages/Home";
-import Ideal from "../pages/Ideal";
+const Home = lazy(() => import("../pages/Home"));
+const Ideal = lazy (() => import("../pages/Ideal"));
 
 function App() {
   return (
     <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Header />
-      <Switch>
-        <Route path="/ideal">
-          <Ideal />
-        </Route>
-        <Route path="">
-          <Home />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/ideal">
+            <Ideal />
+          </Route>
+          <Route path="">
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
