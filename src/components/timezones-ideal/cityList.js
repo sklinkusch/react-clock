4001ebe4c3481797088999ca15ebc5a09217e993
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-12 20:17:33 +0100

    use only cities, no districts in ideal city list

--- src/components/timezones-ideal/cityList.js
+++ src/components/timezones-ideal/cityList.js
@@ -1,5 +1,19 @@
 import citiesRaw from "cities15000-json"
 
-const cities500k = citiesRaw.filter(city => city.population >= 500000)
+const cities500k = citiesRaw.filter(city => Number(city.population) >= 500000)
+const cityList = cities500k.filter(city => {
+  if(city.featureClass === "P") {
+    switch(city.featureCode) {
+      case "PPL":
+      case "PPLA":
+      case "PPLC":
+      case "PPLG":
+        return true
+      default:
+        return false
+    }
+  }
+  return false
+})
 
-export default cities500k
+export default cityList
