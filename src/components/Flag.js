ccdbd8544b74d2ce1f162647023e52823c73882b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-30 07:26:06 +0100

    add flag of Bermuda

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -15,6 +15,8 @@ const Flag = ({code, title}) => {
       return <span title={title} role="img" aria-label={title}>🇦🇼</span>
     case "BB":
       return <span title={title} role="img" aria-label={title}>🇧🇧</span>
+    case "BM":
+      return <span title={title} role="img" aria-label={title}>🇧🇲</span>
     case "BO":
       return <span title={title} role="img" aria-label={title}>🇧🇴</span>
     case "BR":
