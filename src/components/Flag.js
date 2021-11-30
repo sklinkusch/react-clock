9acec2cb2cc0f5848a41884d8193fd945cddd371
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-30 07:00:38 +0100

    add flag of Guatemala

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -15,6 +15,8 @@ const Flag = ({code, title}) => {
       return <span title={title} role="img" aria-label={title}>🇨🇷</span>
     case "EC":
       return <span title={title} role="img" aria-label={title}>🇪🇨</span>
+    case "GT":
+      return <span title={title} role="img" aria-label={title}>🇬🇹</span>
     case "MX":
       return <span title={title} role="img" aria-label={title}>🇲🇽</span>
     case "NU":
