3483df7da0332f1c84577600963ef60094311333
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-03-20 18:00:31 +0100

    new method to filter cities

--- src/components/timezones-ideal/cityList.js
+++ src/components/timezones-ideal/cityList.js
@@ -1,3 +1,14 @@
-import cityList from "../cities500K.json"
+import cityListRaw from "../cities500K.json"
+import cPopulation from "../population"
+
+const cityList = cityListRaw.filter(city => {
+  const { country, population } = city
+  const countryPopulation = cPopulation[country] 
+  const minPopulation = 0.005 * countryPopulation
+  if (population >= minPopulation) {
+    return true
+  }
+  return false
+})
 
 export default cityList
