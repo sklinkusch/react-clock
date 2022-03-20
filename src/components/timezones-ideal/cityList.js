60b1d1e6f059190d0e9dbdb8e3b77fbbf0400c63
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-03-20 10:43:52 +0100

    timezones-ideal does not rely on cities15000-json any more

--- src/components/timezones-ideal/cityList.js
+++ src/components/timezones-ideal/cityList.js
@@ -1,29 +1,3 @@
-import citiesRaw from "cities15000-json"
-
-const cities500k = citiesRaw.filter(city => Number(city.population) >= 500000)
-const cityList = cities500k.filter(city => {
-  if(city.featureClass === "P") {
-    switch(city.featureCode) {
-      case "PPL":
-      case "PPLA":
-      case "PPLC":
-      case "PPLG":
-        return true
-      case "PPLA2":
-        switch(city.asciiname) {
-          case "The Bronx":
-          case "Brooklyn":
-          case "Manhattan":
-          case "Borough of Queens":
-            return false
-          default:
-            return true
-        }
-      default:
-        return false
-    }
-  }
-  return false
-})
+import cityList from "../cities500K.json"
 
 export default cityList
