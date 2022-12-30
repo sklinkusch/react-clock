b7e86d804ae9665f5e7ba301b3e66a325a1815c4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-30 21:36:48 +0100

    add French translations for Indonesia (real)

--- src/components/timezones/Indonesia.js
+++ src/components/timezones/Indonesia.js
@@ -4,79 +4,92 @@ const countryList = [
   {
     country: {
       de: "Indonesien",
-      en: "Indonesia"
+      en: "Indonesia",
+      fr: "Indonésie"
     },
     subdiv: [
       {
         title: {
           de: "Bali",
-          en: "Bali"
+          en: "Bali",
+          fr: "Bali"
         }
       }, 
       {
         title: {
           de: "Ostkalimantan",
-          en: "East Kalimantan"
+          en: "East Kalimantan",
+          fr: "Kalimantan oriental"
         }
       }, 
       {
         title: {
           de: "Ost-Nusa-Tenggara",
-          en: "East Nusa Tenggara"
+          en: "East Nusa Tenggara",
+          fr: "Petites îles de la Sonde orientales"
         }
       }, 
       {
         title: {
           de: "Nordkalimantan",
-          en: "North Kalimantan"
+          en: "North Kalimantan",
+          fr: "Kalimantan du Nord"
         }
       }, 
       {
         title: {
           de: "Südkalimantan",
-          en: "South Kalimantan"
+          en: "South Kalimantan",
+          fr: "Kalimantan du Sud"
         }
       }, 
       {
         title: {
           de: "Zentralsulawesi",
-          en: "Central Sulawesi"
+          en: "Central Sulawesi",
+          fr: "Sulawesi central"
         }
       },
       {
         title: {
           de: "Westsulawesi",
-          en: "West Sulawesi"
+          en: "West Sulawesi",
+          fr: "Sulawesi occidental"
         }
       },
       {
         title: {
           de: "Südsulawesi",
-          en: "South Sulawesi"
+          en: "South Sulawesi",
+          fr: "Sulawesi du Sud"
         }
       },
       {
         title: {
           de: "Südostsulawesi",
-          en: "Southeast Sulawesi"
+          en: "Southeast Sulawesi",
+          fr: "Sulawesi du Sud-Est"
         }
       },
       {
         title: {
           de: "Nordsulawesi",
-          en: "North Sulawesi"
+          en: "North Sulawesi",
+          fr: "Sulawesi du Nord"
         }
       },
       {
         title: {
           de: "Gorontalo",
-          en: "Gorontalo"
+          en: "Gorontalo",
+          fr: "Gorontalo"
         }
       }, 
       {
         title: {
           de: "West-Nusa-Tenggara",
-          en: "West Nusa Tenggara"
+          en: "West Nusa Tenggara",
+          fr: "Petites îles de la Sonde occidentales"
         }
       }
     ],
@@ -87,103 +100,120 @@ const countryList = [
   {
     country: {
       de: "Indonesien",
-      en: "Indonesia"
+      en: "Indonesia",
+      fr: "Indonésie"
     },
     subdiv: [
       {
         title: {
           de: "Banten",
-          en: "Banten"
+          en: "Banten",
+          fr: "Banten"
         }
       },
       {
         title: {
           de: "Ostjava",
-          en: "East Java"
+          en: "East Java",
+          fr: "Java oriental"
         }
       },
       {
         title: {
           de: "Westjava",
-          en: "West Java"
+          en: "West Java",
+          fr: "Java occidental"
         }
       },
       {
         title: {
           de: "Zentraljava",
-          en: "Central Java"
+          en: "Central Java",
+          fr: "Java central"
         }
       },
       {
         title: {
           de: "Jakarta",
-          en: "Jakarta"
+          en: "Jakarta",
+          fr: "Jakarta"
         }
       },
       {
         title: {
           de: "Yogyakarta",
-          en: "Yogyakarta"
+          en: "Yogyakarta",
+          fr: "Yogyakarta"
         }
       },
       {
         title: {
           de: "Bangka-Belitung",
-          en: "Bangka Belitung"
+          en: "Bangka Belitung",
+          fr: "Îles Bangka Belitung"
         }
       },
       {
         title: {
           de: "Bengkulu",
-          en: "Bengkulu"
+          en: "Bengkulu",
+          fr: "Bengkulu"
         }
       },
       {
         title: {
           de: "Jambi",
-          en: "Jambi"
+          en: "Jambi",
+          fr: "Jambi"
         }
       },
       {
         title: {
           de: "Lampung",
-          en: "Lampung"
+          en: "Lampung",
+          fr: "Lampung"
         }
       },
       {
         title: {
           de: "Nordsumatra",
-          en: "North Sumatra"
+          en: "North Sumatra",
+          fr: "Sumatra du Nord"
         }
       },
       {
         title: {
           de: "Riau",
-          en: "Riau"
+          en: "Riau",
+          fr: "Riau"
         }
       },
       {
         title: {
           de: "Riau-Inseln",
-          en: "Riau Islands"
+          en: "Riau Islands",
+          fr: "Îles Riau"
         }
       },
       {
         title: {
           de: "Südsumatra",
-          en: "South Sumatra"
+          en: "South Sumatra",
+          fr: "Sumatra du Sud"
         }
       },
       {
         title: {
           de: "Westsumatra",
-          en: "West Sumatra"
+          en: "West Sumatra",
+          fr: "Sumatra occidental"
         }
       },
       {
         title: {
           de: "Aceh",
-          en: "Aceh"
+          en: "Aceh",
+          fr: "Aceh"
         }
       },
     ],
@@ -194,19 +224,22 @@ const countryList = [
   {
     country: {
       de: "Indonesien",
-      en: "Indonesia"
+      en: "Indonesia",
+      fr: "Indonésie"
     },
     subdiv: [
       {
         title: {
           de: "Zentralkalimantan",
-          en: "Central Kalimantan"
+          en: "Central Kalimantan",
+          fr: "Kalimantan central"
         }
       },
       {
         title: {
           de: "Westkalimantan",
-          en: "West Kalimantan"
+          en: "West Kalimantan",
+          fr: "Kalimantan occidental"
         }
       },
     ],
@@ -217,31 +250,36 @@ const countryList = [
   {
     country: {
       de: "Indonesien",
-      en: "Indonesia"
+      en: "Indonesia",
+      fr: "Indonésie"
     },
     subdiv: [
       {
         title: {
           de: "Maluku",
-          en: "Maluku"
+          en: "Maluku",
+          fr: "Moluques"
         }
       }, 
       {
         title: {
           de: "Nordmaluku",
-          en: "North Maluku"
+          en: "North Maluku",
+          fr: "Moluques du Nord"
         }
       }, 
       {
         title: {
           de: "Papua",
-          en: "Papua"
+          en: "Papua",
+          fr: "Papouasie"
         }
       }, 
       {
         title: {
           de: "Westpapua",
-          en: "West Papua"
+          en: "West Papua",
+          fr: "Papouasie occidentale"
         }
       }
     ],
