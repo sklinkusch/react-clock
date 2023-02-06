087abbf26af9082510aee235a6ea63f6de1104ae
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-02-06 19:03:26 +0100

    add lowercase once again

--- src/components/getLocale.js
+++ src/components/getLocale.js
@@ -1,7 +1,7 @@
 import locales from "./locales.json"
 
 const getPermLocale = (identifier) => {
-  const lang = window.navigator.language
+  const lang = window.navigator.language.substring(0,2).toLowerCase()
   if(locales[identifier].hasOwnProperty(lang)) {
     return locales[identifier][lang]
   }
