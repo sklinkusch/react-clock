674284bd5f13f057a6d415774235b46cfbe584e0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2026-02-08 16:14:48 +0100

    use new custom hook useGetData

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,12 @@
-import React, { useEffect, lazy } from "react";
-import { useDebugState } from "use-named-state";
-import moment from "moment-timezone"
+import React, { lazy } from "react";
 import "../styles/App.css";
 import { getPermLocale } from "../components/getLocale";
+import { useGetData } from "../hooks/useGetData";
 const Clock = lazy(() => import("../components/Clock"));
 /* eslint-disable react-hooks/exhaustive-deps */
 
 export default function Home() {
-  const [lang, setLang] = useDebugState("lang", "")
-  const [allTz, setAllTz] = useDebugState("allTz", [])
-  const [filtVal, setFiltVal] = useDebugState("filterValue", "")
-  const [timezones, setTimezones] = useDebugState("timezones", [])
-  const fetchData = (currentLanguage) => {
-    fetch(`https://worldtime-api.vercel.app/real?lang=${currentLanguage}`)
-    .then(response => response.json())
-    .then(data => {
-      setAllTz(data)
-      prepareZones(data, filtVal, currentLanguage)
-      setInterval(1000, () => prepareZones(allTz, filtVal, currentLanguage))
-    })
-    .catch(error => console.error(error))
-  } 
-  const prepareZones = (data, filterVal = "", currentLanguage) => {
-    const myData = data ? data : allTz
-    const unsortedStates = myData.map(tz => {
-      const Now = moment().utc().format("x")
-      const {country, zone, flag, subdiv = [], cities = [] } = tz
-      const offset = moment().tz(zone).format("Z")
-      const numericOffset = -1 * moment.tz.zone(zone).utcOffset(Now)
-      return { country, zone, flag, offset, numericOffset, subdiv, cities }
-    })
-    const sortedStates = unsortedStates.sort((a,b) => (a.numericOffset - b.numericOffset))
-    const timezoneObject = sortedStates.reduce((acc, curr) => {
-      const obj = {...acc}
-      const { flag: code, offset, country: title, zone, numericOffset, subdiv = [], cities = [] } = curr
-      const city = numericOffset === 0 ? "UTC±00:00" : "UTC" + offset
-      const flag = { code, title, subdiv }
-      if (obj.hasOwnProperty(city)) {
-        obj[city].flags.push(flag)
-        obj[city].cities = obj[city].cities.concat(cities)
-      } else {
-        obj[city] = { city, numericOffset, zone, flags: [flag], cities }
-      }
-      return obj
-    }, {})
-    const timezoneArray = Object.values(timezoneObject).sort((a,b) => (a.numericOffset - b.numericOffset))
-    const filterValue = filterVal ? filterVal : filtVal
-    const filteredStates = filterValue !== ""  ? timezoneArray.filter(tz => {
-      const { flags, cities } = tz
-      const filteredFlags = flags.filter(flag => {
-        const { title: country, subdiv = [] } = flag
-        const title = subdiv.length > 0 ? `${country}: ${subdiv.map(item => item.title).join(", ")}` : country
-        if(filterValue !== ""){
-          if(title.toLowerCase().includes(filterValue.toLowerCase())) return true
-          return false
-        }
-        return true
-      })
-      if(filteredFlags.length > 0) return true
-      const filteredCities = cities.filter(city => city.asciiname.toLowerCase().includes(filterValue.toLowerCase()))
-      if(filteredCities.length > 0) return true
-      return false
-    }) : timezoneArray
-    setTimezones(filteredStates)
-  }
-  useEffect(() => {
-    const currentLanguage = window.navigator.language.substring(0,2).toLowerCase()
-    setLang(currentLanguage)
-    fetchData(currentLanguage)
-  }, [])
+  const { lang, timezones, setFiltVal, prepareZones, allTz } = useGetData('real');
   return (
     <div className="app">
       <div style={{ textAlign: "center", marginBottom: "24px", marginTop: "24px" }}>
--- src/pages/Ideal.js
+++ src/pages/Ideal.js
@@ -1,104 +1,12 @@
-import React, { useEffect, lazy } from "react";
-import { useDebugState } from "use-named-state";
+import React, { lazy } from "react";
 import "../styles/App.css";
-import moment from 'moment-timezone';
 import { getPermLocale } from "../components/getLocale";
+import { useGetData } from "../hooks/useGetData";
 const Clock = lazy(() => import("../components/Clock"))
 /* eslint-disable react-hooks/exhaustive-deps */
 
 export default function Ideal() {
-  const [lang, setLang] = useDebugState("lang", "")
-  const [allTz, setAllTz] = useDebugState("allTz", [])
-  const [filtVal, setFiltVal] = useDebugState("filterValue", "")
-  const [timezones, setTimezones] = useDebugState("timezones", [])
-  const fetchData = (currentLanguage) => {
-    fetch(`https://worldtime-api.vercel.app/ideal?lang=${currentLanguage}`)
-    .then(response => response.json())
-    .then(data => {
-      setAllTz(data)
-      prepareZones(data, filtVal, currentLanguage)
-      setInterval(1000, () => prepareZones(allTz, filtVal, currentLanguage))
-    })
-    .catch(error => console.error(error))
-  } 
-  const prepareZones = (data, filterVal = "", currentLanguage) => {
-    const myData = data ? data : allTz
-    const unsortedStates = myData.map(tz => {
-      const {country, flag, subdiv = [], cities = [], zone = null, utcOffset = null } = tz
-      const Now = moment().utc().format("x")
-      const offset = zone ? moment().tz(zone).format("Z") : -1 * utcOffset
-      const numericOffset = zone ? -1 * moment.tz.zone(zone).utcOffset(Now) : utcOffset
-      return { country, flag, offset, zone, numericOffset, subdiv, cities }
-    })
-    const sortedStates = unsortedStates.sort((a,b) => (a.numericOffset - b.numericOffset))
-    const timezoneObject = sortedStates.reduce((acc, curr) => {
-      const obj = {...acc}
-      const { flag: code, offset, country: title, numericOffset, subdiv = [], cities = [] } = curr
-      const isOffsetNumeric = typeof offset === 'number'
-      const offsetSign = isOffsetNumeric
-        ? offset === 0
-          ? '±'
-          : offset < 0
-            ? '+'
-            : '-'
-        : null
-      const offsetAbs = isOffsetNumeric
-        ? Math.abs(offset)
-        : null
-      const offsetHours = isOffsetNumeric
-        ? Math.floor(offsetAbs / 60)
-        : null
-      const offsetFormattedHours = isOffsetNumeric
-        ? offsetHours < 10
-          ? `0${offsetHours}`
-          : `${offsetHours}`
-        : null
-      const offsetMinutes = isOffsetNumeric
-        ? offsetAbs % 60
-        : null
-      const offsetFormattedMinutes = isOffsetNumeric
-        ? offsetMinutes < 10
-          ? `0${offsetMinutes}`
-          : `${offsetMinutes}`
-        : null
-      const formattedOffset = isOffsetNumeric
-        ? `UTC${offsetSign}${offsetFormattedHours}:${offsetFormattedMinutes}`
-        : `UTC${offset}`
-      const city = formattedOffset
-      const flag = { code, title, subdiv }
-      if (obj.hasOwnProperty(city)) {
-        obj[city].flags.push(flag)
-        obj[city].cities = obj[city].cities.concat(cities)
-      } else {
-        obj[city] = { city, numericOffset, flags: [flag], cities }
-      }
-      return obj
-    }, {})
-    const timezoneArray = Object.values(timezoneObject).sort((a,b) => (a.numericOffset - b.numericOffset))
-    const filterValue = filterVal ? filterVal : filtVal
-    const filteredStates = filterValue !== ""  ? timezoneArray.filter(tz => {
-      const { flags, cities } = tz
-      const filteredFlags = flags.filter(flag => {
-        const { title: country, subdiv = [] } = flag
-        const title = subdiv.length > 0 ? `${country}: ${subdiv.map(item => item.title).join(", ")}` : country
-        if(filterValue !== ""){
-          if(title.toLowerCase().includes(filterValue.toLowerCase())) return true
-          return false
-        }
-        return true
-      })
-      if(filteredFlags.length > 0) return true
-      const filteredCities = cities.filter(city => city.asciiname.toLowerCase().includes(filterValue.toLowerCase()))
-      if(filteredCities.length > 0) return true
-      return false
-    }) : timezoneArray
-    setTimezones(filteredStates)
-  }
-  useEffect(() => {
-    const currentLanguage = window.navigator.language.substring(0,2).toLowerCase()
-    setLang(currentLanguage)
-    fetchData(currentLanguage)
-  }, [])
+  const { lang, timezones, setFiltVal, prepareZones, allTz } = useGetData('ideal');
   return (
     <div className="app">
       <div style={{ textAlign: "center", marginBottom: "24px", marginTop: "24px" }}>
